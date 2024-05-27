c0fe9287e5d05b5f1a16128f92f06133444d351c
nguyenducdo47 <nguyenducdo009@gmail.com>
2024-05-28 01:10:50 +0700

    update btn close zalo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { ref } from "vue";
+
 const showFloatIcon = ref<boolean>(false);
 const isInReading = ref<boolean>(false);
 const isLoading = ref<boolean>(true);
@@ -8,368 +10,407 @@ const route = useRoute();
 
 onMounted(() => {
   isInReading.value = !!route.params.chapterId;
-  document.addEventListener('scroll', () => {
+  document.addEventListener("scroll", () => {
     const heightOffset = document.documentElement.scrollTop;
     showFloatIcon.value = heightOffset > 2000;
   });
   isLoading.value = false;
   setTimeout(() => {
     if (loadingDiv.value) {
-      loadingDiv.value.style.display = 'none';
+      loadingDiv.value.style.display = "none";
     }
   }, 1000);
 });
 
 const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
 watch(route, (route) => {
   isInReading.value = !!route.params.chapterId;
 });
+
+const isVisible = ref(true);
+
+function hideZalo() {
+  isVisible.value = false;
+  setTimeout(() => {
+    isVisible.value = true;
+  }, 30000); // Hiển thị lại sau 3 giây
+}
 </script>
 <style>
-    .zalo-container img {
-        max-width: 100%;
-        height: auto;
-        padding: 5px
+  .zalo-container img {
+    max-width: 100%;
+    height: auto;
+    padding: 5px;
+  }
+
+  .zalo-container {
+    position: fixed;
+    width: 40px;
+    height: 40px;
+    bottom: 110px;
+    z-index: 9999999;
+  }
+
+  .zalo-container {
+    right: 33px;
+  }
+
+  .zalo-container a {
+    display: block;
+  }
+
+  .zalo-container span {
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
+    display: flex;
+    -ms-align-items: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: #1182fc;
+    position: relative;
+  }
+
+  @keyframes zoomIn_zalo {
+    from {
+      opacity: 0;
+      transform: scale3d(0.3, 0.3, 0.3);
     }
 
-    .zalo-container {
-        position: fixed;
-        width: 40px;
-        height: 40px;
-        bottom: 110px;
-        z-index: 9999999
+    50% {
+      opacity: 1;
+    }
+  }
+
+  .zoomIn_zalo {
+    animation-name: zoomIn_zalo;
+  }
+
+  .animated_zalo {
+    animation-duration: 1s;
+    animation-fill-mode: both;
+  }
+
+  .animated_zalo.infinite {
+    animation-iteration-count: infinite;
+  }
+
+  .cmoz-alo-circle {
+    width: 50px;
+    height: 50px;
+    top: -5px;
+    right: -5px;
+    position: absolute;
+    background-color: transparent;
+    -webkit-border-radius: 100%;
+    -moz-border-radius: 100%;
+    border-radius: 100%;
+    border: 2px solid rgba(17, 130, 252, 0.8);
+    opacity: 0.1;
+    border-color: #1182fc;
+    opacity: 0.5;
+  }
+
+  .cmoz-alo-circle-fill {
+    width: 60px;
+    height: 60px;
+    top: -10px;
+    position: absolute;
+    -webkit-transition: all 0.2s ease-in-out;
+    -moz-transition: all 0.2s ease-in-out;
+    -ms-transition: all 0.2s ease-in-out;
+    -o-transition: all 0.2s ease-in-out;
+    transition: all 0.2s ease-in-out;
+    -webkit-border-radius: 100%;
+    -moz-border-radius: 100%;
+    border-radius: 100%;
+    border: 2px solid transparent;
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    -o-transition: all 0.5s;
+    transition: all 0.5s;
+    background-color: rgba(17, 130, 252, 0.45);
+    opacity: 0.75;
+    right: -10px;
+  }
+
+  @-webkit-keyframes pulse_zalo {
+    from {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
     }
 
-    .zalo-container {
-        right: 33px
+    50% {
+      -webkit-transform: scale3d(1.05, 1.05, 1.05);
+      transform: scale3d(1.05, 1.05, 1.05);
     }
 
-    .zalo-container a {
-        display: block
+    to {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+  }
+
+  @keyframes pulse_zalo {
+    from {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
     }
 
-    .zalo-container span {
-        display: -webkit-flex;
-        display: -moz-flex;
-        display: -ms-flex;
-        display: -o-flex;
-        display: flex;
-        -ms-align-items: center;
-        align-items: center;
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        background: #1182fc;
-        position: relative
+    50% {
+      -webkit-transform: scale3d(1.05, 1.05, 1.05);
+      transform: scale3d(1.05, 1.05, 1.05);
     }
 
-    @keyframes zoomIn_zalo {
-        from {
-            opacity: 0;
-            transform: scale3d(.3, .3, .3)
-        }
+    to {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+  }
 
-        50% {
-            opacity: 1
-        }
+  .pulse_zalo {
+    -webkit-animation-name: pulse_zalo;
+    animation-name: pulse_zalo;
+  }
+
+  .hotline-phone-ring-wrap {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    z-index: 999999;
+  }
+
+  .hotline-phone-ring {
+    position: relative;
+    visibility: visible;
+    background-color: transparent;
+    width: 110px;
+    height: 110px;
+    cursor: pointer;
+    z-index: 11;
+    -webkit-backface-visibility: hidden;
+    -webkit-transform: translateZ(0);
+    transition: visibility 0.5s;
+    left: 0;
+    bottom: 0;
+    display: block;
+  }
+
+  .hotline-phone-ring-circle {
+    width: 87px;
+    height: 87px;
+    top: 10px;
+    left: 10px;
+    position: absolute;
+    background-color: transparent;
+    border-radius: 100%;
+    border: 2px solid #e60808;
+    -webkit-animation: phonering-alo-circle-anim 1.2s infinite ease-in-out;
+    animation: phonering-alo-circle-anim 1.2s infinite ease-in-out;
+    transition: all 0.5s;
+    -webkit-transform-origin: 50% 50%;
+    -ms-transform-origin: 50% 50%;
+    transform-origin: 50% 50%;
+    opacity: 0.5;
+  }
+
+  .hotline-phone-ring-circle-fill {
+    width: 57px;
+    height: 57px;
+    top: 25px;
+    left: 25px;
+    position: absolute;
+    background-color: rgba(230, 8, 8, 0.7);
+    border-radius: 100%;
+    border: 2px solid transparent;
+    -webkit-animation: phonering-alo-circle-fill-anim 2.3s infinite ease-in-out;
+    animation: phonering-alo-circle-fill-anim 2.3s infinite ease-in-out;
+    transition: all 0.5s;
+    -webkit-transform-origin: 50% 50%;
+    -ms-transform-origin: 50% 50%;
+    transform-origin: 50% 50%;
+  }
+
+  .hotline-phone-ring-img-circle {
+    background-color: #e60808;
+    width: 33px;
+    height: 33px;
+    top: 37px;
+    left: 37px;
+    position: absolute;
+    background-size: 20px;
+    border-radius: 100%;
+    border: 2px solid transparent;
+    -webkit-animation: phonering-alo-circle-img-anim 1s infinite ease-in-out;
+    animation: phonering-alo-circle-img-anim 1s infinite ease-in-out;
+    -webkit-transform-origin: 50% 50%;
+    -ms-transform-origin: 50% 50%;
+    transform-origin: 50% 50%;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .hotline-phone-ring-img-circle .pps-btn-img {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+  }
+
+  .hotline-phone-ring-img-circle .pps-btn-img img {
+    width: 20px;
+    height: 20px;
+  }
+
+  .hotline-bar {
+    position: absolute;
+    background: rgba(230, 8, 8, 0.75);
+    height: 40px;
+    width: 175px;
+    line-height: 40px;
+    border-radius: 3px;
+    padding: 0 10px;
+    background-size: 100%;
+    cursor: pointer;
+    transition: all 0.8s;
+    -webkit-transition: all 0.8s;
+    z-index: 9;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 50px !important;
+    right: 33px;
+    bottom: 37px;
+  }
+
+  .hotline-bar > a {
+    color: #fff;
+    text-decoration: none;
+    font-size: 15px;
+    font-weight: 700;
+    text-indent: 40px;
+    letter-spacing: 1px;
+    line-height: 40px;
+    font-family: Arial;
+  }
+
+  .hotline-bar > a:active,
+  .hotline-bar > a:hover {
+    color: #fff;
+  }
+
+  @-webkit-keyframes phonering-alo-circle-anim {
+    0% {
+      -webkit-transform: rotate(0) scale(0.5) skew(1deg);
+      -webkit-opacity: 0.1;
     }
 
-    .zoomIn_zalo {
-        animation-name: zoomIn_zalo
+    30% {
+      -webkit-transform: rotate(0) scale(0.7) skew(1deg);
+      -webkit-opacity: 0.5;
     }
 
-    .animated_zalo {
-        animation-duration: 1s;
-        animation-fill-mode: both
+    100% {
+      -webkit-transform: rotate(0) scale(1) skew(1deg);
+      -webkit-opacity: 0.1;
+    }
+  }
+
+  @-webkit-keyframes phonering-alo-circle-fill-anim {
+    0% {
+      -webkit-transform: rotate(0) scale(0.7) skew(1deg);
+      opacity: 0.6;
     }
 
-    .animated_zalo.infinite {
-        animation-iteration-count: infinite
+    50% {
+      -webkit-transform: rotate(0) scale(1) skew(1deg);
+      opacity: 0.6;
     }
 
-    .cmoz-alo-circle {
-        width: 50px;
-        height: 50px;
-        top: -5px;
-        right: -5px;
-        position: absolute;
-        background-color: transparent;
-        -webkit-border-radius: 100%;
-        -moz-border-radius: 100%;
-        border-radius: 100%;
-        border: 2px solid rgba(17, 130, 252, .8);
-        opacity: .1;
-        border-color: #1182fc;
-        opacity: .5
+    100% {
+      -webkit-transform: rotate(0) scale(0.7) skew(1deg);
+      opacity: 0.6;
+    }
+  }
+
+  @-webkit-keyframes phonering-alo-circle-img-anim {
+    0% {
+      -webkit-transform: rotate(0) scale(1) skew(1deg);
     }
 
-    .cmoz-alo-circle-fill {
-        width: 60px;
-        height: 60px;
-        top: -10px;
-        position: absolute;
-        -webkit-transition: all .2s ease-in-out;
-        -moz-transition: all .2s ease-in-out;
-        -ms-transition: all .2s ease-in-out;
-        -o-transition: all .2s ease-in-out;
-        transition: all .2s ease-in-out;
-        -webkit-border-radius: 100%;
-        -moz-border-radius: 100%;
-        border-radius: 100%;
-        border: 2px solid transparent;
-        -webkit-transition: all .5s;
-        -moz-transition: all .5s;
-        -o-transition: all .5s;
-        transition: all .5s;
-        background-color: rgba(17, 130, 252, .45);
-        opacity: .75;
-        right: -10px
+    10% {
+      -webkit-transform: rotate(-25deg) scale(1) skew(1deg);
     }
 
-    @-webkit-keyframes pulse_zalo {
-        from {
-            -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1)
-        }
-
-        50% {
-            -webkit-transform: scale3d(1.05, 1.05, 1.05);
-            transform: scale3d(1.05, 1.05, 1.05)
-        }
-
-        to {
-            -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1)
-        }
+    20% {
+      -webkit-transform: rotate(25deg) scale(1) skew(1deg);
     }
 
-    @keyframes pulse_zalo {
-        from {
-            -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1)
-        }
-
-        50% {
-            -webkit-transform: scale3d(1.05, 1.05, 1.05);
-            transform: scale3d(1.05, 1.05, 1.05)
-        }
-
-        to {
-            -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1)
-        }
+    30% {
+      -webkit-transform: rotate(-25deg) scale(1) skew(1deg);
     }
 
-    .pulse_zalo {
-        -webkit-animation-name: pulse_zalo;
-        animation-name: pulse_zalo
+    40% {
+      -webkit-transform: rotate(25deg) scale(1) skew(1deg);
     }
 
-    .hotline-phone-ring-wrap {
-        position: fixed;
-        bottom: 0;
-        right: 0;
-        z-index: 999999
+    50% {
+      -webkit-transform: rotate(0) scale(1) skew(1deg);
     }
 
-    .hotline-phone-ring {
-        position: relative;
-        visibility: visible;
-        background-color: transparent;
-        width: 110px;
-        height: 110px;
-        cursor: pointer;
-        z-index: 11;
-        -webkit-backface-visibility: hidden;
-        -webkit-transform: translateZ(0);
-        transition: visibility .5s;
-        left: 0;
-        bottom: 0;
-        display: block
+    100% {
+      -webkit-transform: rotate(0) scale(1) skew(1deg);
     }
+  }
 
-    .hotline-phone-ring-circle {
-        width: 87px;
-        height: 87px;
-        top: 10px;
-        left: 10px;
-        position: absolute;
-        background-color: transparent;
-        border-radius: 100%;
-        border: 2px solid #e60808;
-        -webkit-animation: phonering-alo-circle-anim 1.2s infinite ease-in-out;
-        animation: phonering-alo-circle-anim 1.2s infinite ease-in-out;
-        transition: all .5s;
-        -webkit-transform-origin: 50% 50%;
-        -ms-transform-origin: 50% 50%;
-        transform-origin: 50% 50%;
-        opacity: .5
-    }
+  .close-btn {
+    z-index: 9;
+    position: absolute;
+    top: -5px;
+    right: -5px;
+    background-color: #503c3c;
+    color: white;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    font-size: 12px;
+    line-height: 1;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    transform: translate(50%, -50%);
+  }
 
-    .hotline-phone-ring-circle-fill {
-        width: 57px;
-        height: 57px;
-        top: 25px;
-        left: 25px;
-        position: absolute;
-        background-color: rgba(230, 8, 8, .7);
-        border-radius: 100%;
-        border: 2px solid transparent;
-        -webkit-animation: phonering-alo-circle-fill-anim 2.3s infinite ease-in-out;
-        animation: phonering-alo-circle-fill-anim 2.3s infinite ease-in-out;
-        transition: all .5s;
-        -webkit-transform-origin: 50% 50%;
-        -ms-transform-origin: 50% 50%;
-        transform-origin: 50% 50%
-    }
-
-    .hotline-phone-ring-img-circle {
-        background-color: #e60808;
-        width: 33px;
-        height: 33px;
-        top: 37px;
-        left: 37px;
-        position: absolute;
-        background-size: 20px;
-        border-radius: 100%;
-        border: 2px solid transparent;
-        -webkit-animation: phonering-alo-circle-img-anim 1s infinite ease-in-out;
-        animation: phonering-alo-circle-img-anim 1s infinite ease-in-out;
-        -webkit-transform-origin: 50% 50%;
-        -ms-transform-origin: 50% 50%;
-        transform-origin: 50% 50%;
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -ms-flexbox;
-        display: flex;
-        align-items: center;
-        justify-content: center
-    }
-
-    .hotline-phone-ring-img-circle .pps-btn-img {
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -ms-flexbox;
-        display: flex
-    }
-
-    .hotline-phone-ring-img-circle .pps-btn-img img {
-        width: 20px;
-        height: 20px
-    }
-
-    .hotline-bar {
-        position: absolute;
-        background: rgba(230, 8, 8, .75);
-        height: 40px;
-        width: 175px;
-        line-height: 40px;
-        border-radius: 3px;
-        padding: 0 10px;
-        background-size: 100%;
-        cursor: pointer;
-        transition: all .8s;
-        -webkit-transition: all .8s;
-        z-index: 9;
-        box-shadow: 0 14px 28px rgba(0, 0, 0, .25), 0 10px 10px rgba(0, 0, 0, .1);
-        border-radius: 50px !important;
-        right: 33px;
-        bottom: 37px
-    }
-
-    .hotline-bar>a {
-        color: #fff;
-        text-decoration: none;
-        font-size: 15px;
-        font-weight: 700;
-        text-indent: 40px;
-        letter-spacing: 1px;
-        line-height: 40px;
-        font-family: Arial
-    }
-
-    .hotline-bar>a:active,
-    .hotline-bar>a:hover {
-        color: #fff
-    }
-
-    @-webkit-keyframes phonering-alo-circle-anim {
-        0% {
-            -webkit-transform: rotate(0) scale(.5) skew(1deg);
-            -webkit-opacity: .1
-        }
-
-        30% {
-            -webkit-transform: rotate(0) scale(.7) skew(1deg);
-            -webkit-opacity: .5
-        }
-
-        100% {
-            -webkit-transform: rotate(0) scale(1) skew(1deg);
-            -webkit-opacity: .1
-        }
-    }
-
-    @-webkit-keyframes phonering-alo-circle-fill-anim {
-        0% {
-            -webkit-transform: rotate(0) scale(.7) skew(1deg);
-            opacity: .6
-        }
-
-        50% {
-            -webkit-transform: rotate(0) scale(1) skew(1deg);
-            opacity: .6
-        }
-
-        100% {
-            -webkit-transform: rotate(0) scale(.7) skew(1deg);
-            opacity: .6
-        }
-    }
-
-    @-webkit-keyframes phonering-alo-circle-img-anim {
-        0% {
-            -webkit-transform: rotate(0) scale(1) skew(1deg)
-        }
-
-        10% {
-            -webkit-transform: rotate(-25deg) scale(1) skew(1deg)
-        }
-
-        20% {
-            -webkit-transform: rotate(25deg) scale(1) skew(1deg)
-        }
-
-        30% {
-            -webkit-transform: rotate(-25deg) scale(1) skew(1deg)
-        }
-
-        40% {
-            -webkit-transform: rotate(25deg) scale(1) skew(1deg)
-        }
-
-        50% {
-            -webkit-transform: rotate(0) scale(1) skew(1deg)
-        }
-
-        100% {
-            -webkit-transform: rotate(0) scale(1) skew(1deg)
-        }
-    }
-
+  #zalo-container.hidden {
+    display: none;
+  }
 </style>
 
 <template>
-   <div class="zalo-container"> 
-    <a id="zalo-btn" href="https://zalo.me/0879292571" target="_blank" rel="noopener noreferrer nofollow">
-        <div class="animated_zalo infinite zoomIn_zalo cmoz-alo-circle"></div>
-        <div class="animated_zalo infinite pulse_zalo cmoz-alo-circle-fill"></div> 
-        <span><img src="/img/zalo-icon.png" alt="Contact Me on Zalo"></span>
-    </a> 
+  <div class="zalo-container" v-show="isVisible">
+    <div class="close-btn" @click="hideZalo">X</div>
+    <a
+      id="zalo-btn"
+      href="https://zalo.me/0879292571"
+      target="_blank"
+      rel="noopener noreferrer nofollow"
+    >
+      <div class="animated_zalo infinite zoomIn_zalo cmoz-alo-circle"></div>
+      <div class="animated_zalo infinite pulse_zalo cmoz-alo-circle-fill"></div>
+      <span><img src="/img/zalo-icon.png" alt="Contact Me on Zalo" /></span>
+    </a>
   </div>
+
   <Navbar v-show="!isInReading" />
   <slot />
   <div
